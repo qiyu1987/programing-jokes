4ccf6f0d83814bb2d21b9948dcd43d7fb8517f79
qiyu1987 <qiyu1987@gmail.com>
2019-12-05 23:40:38 +0100

    add  header

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,18 +11,20 @@ function Home(props) {
 		}, 3000)
 	})
 	return (
-		<div className="container d-flex justify-content-center">
-			<div className="jumbotron my-5">
-				<h1 className="setup">{props.joke.setup}</h1>
-				{reveal ? <h2 className="punchline">{props.joke.punchline}</h2> : null}
+		<div>
+			<div className="container d-flex justify-content-center mt-5">
+				<h1>Programing Joke</h1>
+			</div>
+			<div className="container d-flex justify-content-center">
+				<div className="jumbotron my-5">
+					<h2 className="setup">{props.joke.setup}</h2>
+					{reveal ? (
+						<h3 className="punchline">{props.joke.punchline}</h3>
+					) : null}
+				</div>
 			</div>
 		</div>
 	)
-	// 	<div class="container h-100 d-flex justify-content-center">
-	//     <div class="jumbotron my-auto">
-	//       <h1 class="display-3">Hello, world!</h1>
-	//     </div>
-	//  </div>
 }
 const mapStateToProps = state => ({
 	joke: state.joke
