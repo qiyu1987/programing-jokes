85dbd15e223d5039a1633e1a152ddef97ddf442b
qiyu1987 <qiyu1987@gmail.com>
2019-12-12 16:42:15 +0100

    cleanup signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,31 +4,27 @@ import { connect } from "react-redux"
 import { signUp } from "../actions/auth"
 
 function Signup(props) {
-	const { register, handleSubmit, watch, errors } = useForm()
+	const { register, handleSubmit, errors } = useForm()
 	const onSubmit = data => {
-		console.log(data)
 		props.signUp(data)
 	}
-
-	console.log(watch("username")) // watch input value by passing the name of it
-
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
 			{/* register your input into the hook by invoking the "register" function */}
+			{/* include validation with required or other standard HTML validation rules */}
 			<input
 				name="username"
 				defaultValue="username"
 				ref={register({ required: true })}
 			/>
+			{/* errors will return when field validation fails  */}
 			{errors.username && <span>username is required</span>}
 
-			{/* include validation with required or other standard HTML validation rules */}
 			<input
 				name="password"
 				defaultValue="password"
 				ref={register({ required: true })}
 			/>
-			{/* errors will return when field validation fails  */}
 			{errors.password && <span>password is required</span>}
 
 			<input type="submit" />
